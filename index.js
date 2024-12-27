f7dd887bc13ca80d4d469943469b606c480100a7
Olivier Lefevre <olivierlefevre2035@gmail.com>
2024-12-27 08:37:04 -0500

    Everything but node modules

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { readFile } = require('fs');
+const app = express();
+
+app.get('/', (request, response) => {
+
+  readFile('./mainpage.html', 'utf8', (err, html) => {
+
+    if (err) {
+
+      response.status(500).send('sorry out of order')
+
+    }
+
+    response.send(html);
+
+  })
+
+});
+
+app.listen(process.env.PORT || 3000, () => console.log('App available on port 3000'))
---
+++ mainpage.js
@@ -0,0 +1,48 @@
+const cytoscape = window.cytoscape;
+cytoscape({
+    container: document.getElementById('cy'),
+  
+    elements: [
+        { data: { id: 'a' } },
+        { data: { id: 'b' } },
+        { data: { id: 'c' } },
+        { data: { id: 'd' } },
+        {
+          data: {
+            id: 'ab',
+            source: 'a',
+            target: 'b'
+          }
+        },
+        {
+            data: {
+              id: 'cb',
+              source: 'c',
+              target: 'b'
+            }
+          },
+          {
+            data: {
+              id: 'db',
+              source: 'd',
+              target: 'b'
+            }
+          },
+          {
+            data: {
+              id: 'cd',
+              source: 'c',
+              target: 'd'
+            }
+          },],
+
+    style: [
+        {
+            selector: 'node',
+            style: {
+                shape: 'hexagon',
+                'background-color': 'red'
+            }
+        }] 
+  
+  });
